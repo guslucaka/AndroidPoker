256fc25eae21dbe9b60b077552a7b44d6684dba6
guslovger <zejrik5@yahoo.se>
2018-12-18 19:12:30 +0100

    Gjorde om blinds-strängarna till intar.

--- app/src/main/java/se/juneday/throwaway/androidpokerclient2/NewHandActivity.java
+++ app/src/main/java/se/juneday/throwaway/androidpokerclient2/NewHandActivity.java
@@ -2,9 +2,11 @@ package se.juneday.throwaway.androidpokerclient2;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.support.annotation.IntDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -22,6 +24,9 @@ public class NewHandActivity extends AppCompatActivity {
 	final int[] month = new int[1];
 	final int[] dayOfMonth = new int[1];
 	final Calendar[] calendar = new Calendar[1];
+	public Spinner dropdownBlinds;
+	public static int blindLevel;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -78,8 +83,7 @@ public class NewHandActivity extends AppCompatActivity {
 			Spinner dropdownLocation = findViewById(R.id.spinner1);
 			//create a list of items for the spinner.
 			String[] locations = new String[]{"Location", "Göteborg", "Stockholm", "Malmö", "Sundsvall"};
-			//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-			//There are multiple variations of this, but this is the basic variant.
+			//create an adapter to describe how the items are displayed.
 			ArrayAdapter<String> locationAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, locations);
 			//set the spinners adapter to the previously created one.
 			dropdownLocation.setAdapter(locationAdapter);
@@ -89,7 +93,7 @@ public class NewHandActivity extends AppCompatActivity {
 			ArrayAdapter<String> gametypeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, gametypes);
 			dropdownGametype.setAdapter(gametypeAdapter);
 
-			Spinner dropdownBlinds = findViewById(R.id.spinner3);
+			dropdownBlinds = findViewById(R.id.spinner3);
 			String[] blinds = new String[]{"Blinds", "10/10", "25/25", "50/50"};
 			ArrayAdapter<String> blindsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, blinds);
 			dropdownBlinds.setAdapter(blindsAdapter);
@@ -100,7 +104,57 @@ public class NewHandActivity extends AppCompatActivity {
 			onDateButtonClick();
 			onEditPlayersClick();
 			onNextClick();
-
+			onBlindSpinnerSelected();
 		}
+
+		private void onBlindSpinnerSelected(){
+			dropdownBlinds.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+					blindLevel = convertBlindsToInt();
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> parent) {
+					//Not needed
+				}
+			});
+		}
+
+	@IntDef({Blinds.BLINDS_DEFAULT, Blinds.BLINDS_FIRST, Blinds.BLINDS_SECOND, Blinds.BLINDS_THIRD}) // Denotes that the annotated element is of integer type.
+	public @interface Blinds {
+		int BLINDS_DEFAULT = 0;
+		int BLINDS_FIRST = 1;
+		int BLINDS_SECOND = 2;
+		int BLINDS_THIRD = 3;
+
+	}
+
+	/**
+	 * Helper method to convert strings of blinds to ints.
+	 *
+	 * @return The mapped int.
+	 */
+	public int convertBlindsToInt() {
+		String selectedBlinds = dropdownBlinds.getSelectedItem().toString();
+
+		int selectedBlind = 0;
+
+		switch (selectedBlinds) {
+			case  "Blinds" :
+				selectedBlind = Blinds.BLINDS_FIRST;
+				break;
+			case "10/10" :
+				selectedBlind = Blinds.BLINDS_FIRST;
+				break;
+			case "25/25":
+				selectedBlind = Blinds.BLINDS_SECOND;
+				break;
+			case "50/50":
+				selectedBlind = Blinds.BLINDS_THIRD;
+				break;
+		}
+		return selectedBlind;
+	}
 
 	}

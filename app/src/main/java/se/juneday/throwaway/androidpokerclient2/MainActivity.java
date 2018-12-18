62ab8bed65fa59f7c6b21f870787869a85cefaa7
Lucas Karlsson <guslucaka@student.gu.se>
2018-12-18 14:09:21 +0100

    First Commit

---
+++ app/src/main/java/se/juneday/throwaway/androidpokerclient2/MainActivity.java
@@ -0,0 +1,26 @@
+package se.juneday.throwaway.androidpokerclient2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button newHandButton = findViewById(R.id.NewHandButton);
+        newHandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewHandActivity.class);
+                startActivity(intent);
+                //test
+            }
+        });
+    }
+}

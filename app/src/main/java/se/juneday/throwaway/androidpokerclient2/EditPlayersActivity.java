ed3fcb362bc5f68e2f771e88afeb0753af5b08d0
guslovger <zejrik5@yahoo.se>
2018-12-19 15:36:15 +0100

    Fixat spinner för amount of players via en array i resources

--- app/src/main/java/se/juneday/throwaway/androidpokerclient2/EditPlayersActivity.java
+++ app/src/main/java/se/juneday/throwaway/androidpokerclient2/EditPlayersActivity.java
@@ -1,7 +1,12 @@
 package se.juneday.throwaway.androidpokerclient2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class EditPlayersActivity extends AppCompatActivity {
 
@@ -9,7 +14,29 @@ public class EditPlayersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editplayers);
+        initViews();
     }
 
+    void initViews(){
+
+        onSavePlayersclick();
+
+        Spinner amountOfPlayers = findViewById(R.id.player_amount_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.player_amount, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        amountOfPlayers.setAdapter(adapter);
+    }
+
+    private void onSavePlayersclick(){
+        final Button button = findViewById(R.id.save_players_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditPlayersActivity.this, NewHandActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }

c58c05b250fd2f367be9c1d86cf5a861231b9044
Lucas Karlsson <guslucaka@student.gu.se>
2018-12-19 13:38:54 +0100

    fixat spinner

--- app/src/main/java/se/juneday/throwaway/androidpokerclient2/PreflopActivity.java
+++ app/src/main/java/se/juneday/throwaway/androidpokerclient2/PreflopActivity.java
@@ -1,9 +1,14 @@
 package se.juneday.throwaway.androidpokerclient2;
 
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PreflopActivity extends AppCompatActivity {
 
@@ -21,8 +26,29 @@ public class PreflopActivity extends AppCompatActivity {
         //create a list of items for the spinner.
         String[] players = new String[]{"Players", "Erik", "Lucas", "Sven"};
         //create an adapter to describe how the items are displayed.
-        ArrayAdapter<String> locationAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, players);
+        ArrayAdapter<String> locationAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, players) {
+            @Override
+            public boolean isEnabled(int position){
+                return position != 0;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
         //set the spinners adapter to the previously created one.
+        locationAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         dropdownLocation.setAdapter(locationAdapter);
     }
 }
